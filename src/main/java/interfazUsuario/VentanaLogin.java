aa399eea020dbe369f9c813dc08be0e916e3281e
Marcos <mpineirotroncoso@danielcastelao.org>
2024-05-03 11:22:02 +0200

    Añade etiquetas, ventana sin bordes y deslizable

--- src/main/java/interfazUsuario/VentanaLogin.java
+++ src/main/java/interfazUsuario/VentanaLogin.java
@@ -1,20 +1,50 @@
 package interfazUsuario;
 
 import interfazUsuario.resources.CuadroDeTexto;
+import interfazUsuario.resources.Etiqueta;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class VentanaLogin extends JFrame {
     private CuadroDeTexto cuadroDeTexto;
+    private Point initialClick;
     //TODO: Añadir cuadros de texto y botones en la ventana
     public VentanaLogin() {
 
         // Mostrar todos los componentes de la ventana
         createAndShowGUI();
 
-        setSize(500, 400);
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                initialClick = e.getPoint();
+                getComponentAt(initialClick);
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                // Localización de la ventana
+                int thisX = getLocation().x;
+                int thisY = getLocation().y;
+
+                // Determinar cuánto se ha movido el ratón desde el click inicial
+                int xMoved = e.getX() - initialClick.x;
+                int yMoved = e.getY() - initialClick.y;
+
+                // Mover la ventana a la nueva posición
+                int X = thisX + xMoved;
+                int Y = thisY + yMoved;
+                setLocation(X, Y);
+            }
+        });
+
+        setSize(500, 350);
         setLayout(null);
+        setLocationRelativeTo(null);
+        setUndecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setTitle("Iniciar Sesión");
@@ -31,8 +61,16 @@ public class VentanaLogin extends JFrame {
         pwtxt.setBorder(BorderFactory.createLineBorder(Color.black, 2));
         pwtxt.setBounds(75, 200, 350, 40);
 
+        JLabel usernamelbl = new Etiqueta("Usuario");
+        usernamelbl.setBounds(75, 65, 100, 40);
+
+        JLabel pwlbl = new Etiqueta("Contraseña");
+        pwlbl.setBounds(75, 165, 500, 40);
+
         this.add(usernametxt);
         this.add(pwtxt);
+        this.add(usernamelbl);
+        this.add(pwlbl);
 
     }
 }
