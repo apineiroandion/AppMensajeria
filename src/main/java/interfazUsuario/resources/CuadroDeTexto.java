49e4936ef7cb58420ec793cbae9cfbe0ec2014d4
Marcos <mpineirotroncoso@danielcastelao.org>
2024-05-03 11:16:31 +0200

    Ajustar tamaño y color letra TextField

--- src/main/java/interfazUsuario/resources/CuadroDeTexto.java
+++ src/main/java/interfazUsuario/resources/CuadroDeTexto.java
@@ -7,7 +7,8 @@ public class CuadroDeTexto extends JTextField{
 
     public CuadroDeTexto(int ancho, int alto) {
         setSize(ancho, alto);
-        setForeground(new Color(255, 255, 255));
+        setFont(new Font("Arial", 0, 15));
+        setForeground(new Color(175, 175, 175));
         setBackground(new Color(40, 40, 40));
     }
 }
