f2c6fe67a4ad10fb6848e075dddd2eea6609d45f
Marcos <mpineirotroncoso@danielcastelao.org>
2024-05-06 11:31:57 +0200

    constructor TextField sin borderColor

--- src/main/java/interfazUsuario/resources/CuadroDeTexto.java
+++ src/main/java/interfazUsuario/resources/CuadroDeTexto.java
@@ -7,7 +7,22 @@ import java.awt.*;
 
 public class CuadroDeTexto extends JTextField{
     /**
-     * Cuadro de texto
+     * Cuadro de texto con borderColor que cambia de color al seleccionarlo
+     * @param ancho ancho del JTextField
+     * @param alto alto del JTextField
+     * @param borderColor color del borde
+     */
+    public CuadroDeTexto(int ancho, int alto, Color borderColor) {
+        setSize(ancho, alto);
+        setFont(new Font("Arial", 0, 15));
+        setBorder(BorderFactory.createLineBorder(borderColor, 2));
+        setForeground(new Color(175, 175, 175));
+        setBackground(new Color(40, 40, 40));
+        addFocusListener(new EventoColor(this, borderColor, Color.white));
+    }
+
+    /**
+     * Cuadro de texto sin borderColor
      * @param ancho ancho del JTextField
      * @param alto alto del JTextField
      */
@@ -16,6 +31,5 @@ public class CuadroDeTexto extends JTextField{
         setFont(new Font("Arial", 0, 15));
         setForeground(new Color(175, 175, 175));
         setBackground(new Color(40, 40, 40));
-        addFocusListener(new EventoColor(this, Color.BLACK, Color.white));
     }
 }
