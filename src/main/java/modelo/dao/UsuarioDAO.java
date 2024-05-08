fbe5ee195635b019f19efac15f65e3537a349182
Angel <apineiroandion@danielcastelao.org>
2024-05-08 12:35:15 +0200

    actualizado TODO

--- src/main/java/modelo/dao/UsuarioDAO.java
+++ src/main/java/modelo/dao/UsuarioDAO.java
@@ -3,6 +3,7 @@ package modelo.dao;
 import java.sql.Statement;
 
 public class UsuarioDAO implements Consulta{
+    //TODO: Cambiar a aprametro String, y hacer static final con los strings
     @Override
     public String setQuery() {
         // Crear un Statement para ejecutar consultas
