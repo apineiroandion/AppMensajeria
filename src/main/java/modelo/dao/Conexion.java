94c3d1e8651f0d223a3f2675544ef040c1b2f20b
Angel <apineiroandion@danielcastelao.org>
2024-05-08 12:15:31 +0200

    metodos de conexion

--- src/main/java/modelo/dao/Conexion.java
+++ src/main/java/modelo/dao/Conexion.java
@@ -1,8 +1,6 @@
 package modelo.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * ESta calse realiza la conexion con la base de datos
@@ -45,15 +43,56 @@ public class Conexion {
     /**
      * Clase que inicia y cierra la conexion con la base de datos
      */
-    public static void conexionInit() {
+    public static void conexionInit(Consulta consulta) {
         try {
             Connection con = DriverManager.getConnection(conexion.getUrl(), conexion.getUser(), conexion.getPassword());
             System.out.println("Conectado");
-            //Introducir consultas
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(consulta.setQuery());
+            // Procesar los resultados de la consulta
+            while (rs.next()) {
+                // Accede a los datos de cada fila
+                int id = rs.getInt("idu");
+                String nombre = rs.getString("userName");
+                // Hacer algo con los datos...
+                System.out.println("ID: " + id + ", Nombre: " + nombre);
+            }
+            // Cerrar ResultSet, Statement y conexión
+            rs.close();
+            stmt.close();
             con.close(); // Cierra la conexión
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        System.out.println("Fin metodo");
+    }
+    //TODO : Actualizar metodo para que inserte cosas
+    public static void insertar() {
+        try {
+        Connection con = DriverManager.getConnection(conexion.getUrl(), conexion.getUser(), conexion.getPassword());
+        System.out.println("Conectado");
+        String insert = "insert into usuario (userName, firstName, surname, password) VALUES (?, ?, ?, ?)";
+
+        PreparedStatement pstmt = con.prepareStatement(insert);
+        pstmt.setString(1, "userName");
+        pstmt.setString(2, "firstName");
+        pstmt.setString(3, "surname");
+        pstmt.setString(4, "password");
+
+            // Ejecutar la consulta de inserción
+            int filasAfectadas = pstmt.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                System.out.println("Inserción exitosa");
+            } else {
+                System.out.println("La inserción no tuvo éxito");
+            }
+            pstmt.close();
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     // Getters y Setters
