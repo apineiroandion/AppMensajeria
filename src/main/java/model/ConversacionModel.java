bbfdf5b411bba05a0f401fa4a8cc2349243c2888
Angel <apineiroandion@danielcastelao.org>
2024-05-10 12:06:35 +0200

    refactor( metodos del controller repartido0s en sus respectivos model)

--- src/main/java/model/ConversacionModel.java
+++ src/main/java/model/ConversacionModel.java
@@ -1,4 +1,25 @@
 package model;
 
 public class ConversacionModel {
+    /**
+     * Crea una nueva conversacion entre dos usuarios
+     * @param usuarioUno
+     * @param usuarioDos
+     * @return conversacion nueva con usuario seleccionado
+     */
+    public Conversacion newConversacion(Usuario usuarioUno, Usuario usuarioDos) {
+        Conversacion conversacion = new Conversacion(usuarioUno, usuarioDos);
+        return conversacion;
+    }
+    /**
+     * Añade la conversacion a los array list de los dos usuarios, recomemçndable usara como parametro el
+     * metodo newConversacion
+     * @param conversacion
+     */
+    public void addConversacion(Conversacion conversacion) {
+        //añado la conversacion seleccionada al array list de conversaciones de este usuario
+        conversacion.getUsuarioUno().getConversaciones().add(conversacion);
+        //añado la conversacion al arry list del ususario dos
+        conversacion.getUsuarioDos().getConversaciones().add(conversacion);
+    }
 }
--- src/main/java/model/UsuarioModel.java
+++ src/main/java/model/UsuarioModel.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 public class UsuarioModel {
     private ArrayList<Usuario> usuarios;
 
-
+    /**
+     * Metodo que crea un nuevo ususario
+     * @param userName
+     * @param firstName
+     * @param surName
+     * @param password
+     * @return el usuario creado
+     */
+    public Usuario nuevoUsuario(String userName, String firstName, String surName, String password){
+        return new Usuario(userName, firstName, surName, password);
+    }
 
     public UsuarioModel(ArrayList<Usuario> usuarios) {
         this.usuarios = usuarios;
