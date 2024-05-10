b8e6116f830c0c371d7b9a85f8444ea057ac0b5f
Marcos <mpineirotroncoso@danielcastelao.org>
2024-05-10 11:44:58 +0200

    Refactor panels

---
+++ src/main/java/view/panels/BottomLeftPanel.java
@@ -0,0 +1,12 @@
+package view.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BottomLeftPanel extends JPanel {
+    public BottomLeftPanel() {
+        setLayout(new GridBagLayout());
+        setPreferredSize(new Dimension(250,100));
+        setBackground(new Color(40,40,40));
+    }
+}
