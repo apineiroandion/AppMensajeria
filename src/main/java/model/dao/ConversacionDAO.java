9de8ebda88c0473eac4ee5b16a060646fd6392f5
Angel <apineiroandion@danielcastelao.org>
2024-05-10 12:37:17 +0200

    empizo metodo insertar convesacion

--- src/main/java/model/dao/ConversacionDAO.java
+++ src/main/java/model/dao/ConversacionDAO.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import model.Conversacion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConversacionDAO {
     //TODO : Metodos de transferencaia con la DB
+
+    public static void insertarConversacion(Conversacion conversacion) {
+
+    }
 }
