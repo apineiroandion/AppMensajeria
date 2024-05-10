b281fd8f27f3b3cb6de75266e60d75661f21c44f
Marcos <mpineirotroncoso@danielcastelao.org>
2024-05-10 12:38:52 +0200

    Otro favor a mis compañeros ingleses 🦅 🇬🇧

--- src/main/java/view/resources/Label.java
+++ src/main/java/view/resources/Label.java
@@ -5,13 +5,13 @@ import view.resources.events.EventLink;
 import javax.swing.*;
 import java.awt.*;
 
-public class Etiqueta extends JLabel {
+public class Label extends JLabel {
     /**
      * Etiqueta de texto normal
      * @param texto String del contenido
      * @param tamanoFuente tamaño del texto
      */
-    public Etiqueta(String texto, int tamanoFuente) {
+    public Label(String texto, int tamanoFuente) {
         super(texto);
         setFont(new java.awt.Font("Arial", Font.PLAIN, tamanoFuente));
         setForeground(new java.awt.Color(255, 255, 255));
@@ -23,7 +23,7 @@ public class Etiqueta extends JLabel {
      * @param tamanoFuente tamaño del texto
      * @param url url del enlace
      */
-    public Etiqueta(String texto, int tamanoFuente, String url) {
+    public Label(String texto, int tamanoFuente, String url) {
         super(texto);
         setFont(new java.awt.Font("Arial", Font.PLAIN, tamanoFuente));
         setForeground(new java.awt.Color(255, 255, 255));
