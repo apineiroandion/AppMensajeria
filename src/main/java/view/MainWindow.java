9c8dfd03339abd331c4f5760c2a0c32e7c70b933
Marcos <mpineirotroncoso@danielcastelao.org>
2024-05-15 12:18:27 +0200

    Cuadro mensaje y boton enviar

--- src/main/java/view/MainWindow.java
+++ src/main/java/view/MainWindow.java
@@ -1,8 +1,8 @@
 package view;
 
-import view.panels.BottomLeftPanel;
-import view.panels.RightChatPanel;
-import view.panels.LeftPanel;
+import view.panels.*;
+import view.resources.GenericButton;
+import view.resources.GenericTextField;
 import view.resources.Label;
 
 import javax.swing.*;
@@ -12,10 +12,21 @@ import java.awt.*;
  * Frame ventana principal
  */
 public class MainWindow extends JFrame {
+    // Paneles
     private LeftPanel leftPanel;
     private BottomLeftPanel bottomLeftPanel;
     private RightChatPanel rightChatPanel;
+    private RightBottomPanel rightBottomPanel;
+    private GridBagConstraints gbcrightBottomPanel;
+
+    // Labels
     private Label userNamelbl;
+
+    // TextFields
+    private GenericTextField chatmsg;
+
+    // Buttons
+    private GenericButton sendButton;
 
     public MainWindow() {
         setLayout(new BorderLayout());
@@ -23,6 +34,10 @@ public class MainWindow extends JFrame {
         crearPanels();
         // Mostrar todos los labels del frame
         crearLabels();
+        // Mostrar todos los textfields del frame
+        crearTextFields();
+        // Mostrar todos los buttons del frame
+        crearButtons();
         setSize(800, 600);
         setTitle("Chats");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -51,12 +66,50 @@ public class MainWindow extends JFrame {
          */
         rightChatPanel = new RightChatPanel();
         add(rightChatPanel, BorderLayout.CENTER);
+
+        /**
+         * rightBottomPanel GridBagLayout
+         */
+        rightBottomPanel = new RightBottomPanel();
+        rightChatPanel.add(rightBottomPanel, BorderLayout.SOUTH);
+        gbcrightBottomPanel = new GridBagConstraints();
+        gbcrightBottomPanel.gridx = 0;
+        gbcrightBottomPanel.gridy = 0;
+        gbcrightBottomPanel.weightx = 0.8;
+        gbcrightBottomPanel.weighty = 1;
+        gbcrightBottomPanel.fill = GridBagConstraints.BOTH;
     }
     /**
      * Iniciar todos los labels del JFrame
      */
     private void crearLabels(){
+        // Label UserName
         userNamelbl = new Label("UserName",15);
         bottomLeftPanel.add(userNamelbl);
+    }
+    /**
+     * Iniciar todos los textfields del JFrame
+     */
+    private void crearTextFields() {
+        // JTextField chatmsg
+        chatmsg = new GenericTextField(100,50,Color.BLACK);
+        chatmsg.setPreferredSize(new Dimension(chatmsg.getWidth(), 50));
+        rightBottomPanel.add(chatmsg,gbcrightBottomPanel);
+    }
+    /**
+     * Iniciar todos los buttons del JFrame
+     */
+    private void crearButtons() {
+        // JButton enviar
+        sendButton = new GenericButton("Enviar");
+        sendButton.setMaximumSize(new Dimension(25,50));
+        sendButton.setMinimumSize(new Dimension(25,50));
+        sendButton.setPreferredSize(new Dimension(50,50));
+        gbcrightBottomPanel.gridx = 1;
+        gbcrightBottomPanel.gridy = 0;
+        gbcrightBottomPanel.weightx = 0.2;
+        gbcrightBottomPanel.weighty = 1;
+        gbcrightBottomPanel.fill = GridBagConstraints.BOTH;
+        rightBottomPanel.add(sendButton,gbcrightBottomPanel);
     }
 }
---
+++ src/main/java/view/panels/RightBottomPanel.java
@@ -0,0 +1,11 @@
+package view.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RightBottomPanel extends JPanel {
+    public RightBottomPanel() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+    }
+}
