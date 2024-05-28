d6a3568b7e6fc5174ad9426dd7123ee46d0c3d84
Angel <apineiroandion@danielcastelao.org>
2024-05-28 13:08:44 +0200

    creo clase thread para que actualize los mensajes como leidos

---
+++ src/main/java/controller/UpdateMensajesThread.java
@@ -0,0 +1,9 @@
+package controller;
+
+/**
+ * Esta clase es un thread que marca los mensajes de la conversacion seleccionada
+ * como leidos cuando el usuario logeado sea distinto al usurio que envio el mensaje
+ */
+public class UpdateMensajesThread extends Thread{
+
+}
